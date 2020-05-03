8045df0059b1e02ba68f1d4633d8548b96a7c0b0
TuanDH94 <tuandh.ttud@gmail.com>
2020-05-03 20:23:35 +0700

    Delete test.py

--- test.py
+++
@@ -1,6 +0,0 @@
-import pandas as pd
-submit = pd.read_csv('input/sample_submission.csv')
-submit['label'] = 1.0
-submit.to_csv('submission1.csv')
-submit['label'] = 0.0
-submit.to_csv('submission2.csv')
